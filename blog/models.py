4561451c885fc2b308f7d1b0ec7a6aa183166341
davidkartuzinski <david@kaidawei.me>
2021-06-05 00:15:39 +0200

    Learned about views within the app, I had to replace the bootstrap files as I made a mistake about the files. I got everything running.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
 
 # Create your models here.
+
+# class Post(models.Model):
+#     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     title = models.CharField(max_length=200)
+#     post_slug = models.SlugField(max_length=60, unique=True)
+#     header_image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
--- elliesite/urls.py
+++ elliesite/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
+
+# https://overiq.com/django-1-10/handling-media-files-in-django/
 urlpatterns = [
+    url(r'^blog/', include('blog.urls')),
+    url(r'^l/', include('landing_pages.urls')),
     path('admin/', admin.site.urls),
 ]
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
