1c1dcd54ca9ab556af74a8cdb8229cac632d9f52
davidkartuzinski <david@kaidawei.me>
2021-06-04 02:15:02 +0200

    Somehow when I refactored the commits I lost django, so I had to quickly just reinstall django. Easy (at this point).

--- elliesite/wsgi.py
+++ elliesite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for elliewebsite_django project.
+WSGI config for elliesite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elliewebsite_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elliesite.settings')
 
 application = get_wsgi_application()
