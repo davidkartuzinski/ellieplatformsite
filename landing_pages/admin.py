b961f14c9dc46c484c7d6229c38e6be6aa189a32
Bob Belderbos <bobbelderbos@gmail.com>
2021-06-04 14:05:08 +0200

    coding session

--- landing_pages/admin.py
+++ landing_pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from landing_pages.models import (
+    Tag, Campaign, Page)
+
+
+admin.site.register(Tag)
+admin.site.register(Campaign)
+admin.site.register(Page)
