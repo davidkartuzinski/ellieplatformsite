ec7e8ce5ebb40ac9381daea5aafaf4777c1577ea
davidkartuzinski <david@kaidawei.me>
2021-06-06 13:01:48 +0200

    updated the landing_pages model and added the blog models. edit the gitignore to remove a few more types of files.

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from tinymce.models import HTMLField
 
 
 # Create your models here.
 
-# class Post(models.Model):
-#     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     title = models.CharField(max_length=200)
-#     post_slug = models.SlugField(max_length=60, unique=True)
-#     header_image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    post_slug = models.SlugField(max_length=60, unique=True)
+    header_image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    post_content = HTMLField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+    meta_description = models.TextField(max_length=160, default='meta description')
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
