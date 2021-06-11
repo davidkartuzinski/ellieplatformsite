f1424046b11be287fa3a5a606b2127cf9100630d
davidkartuzinski <david@kaidawei.me>
2021-06-11 02:02:02 +0200

    added pagination to additional pages if needed on blog roll page and if there are additional posts to list

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,21 @@
+from django.core import paginator
 from django.shortcuts import render
 from django.utils import timezone
 from django.conf import settings
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'base_dir': settings.BASE_DIR, 'posts': posts})
+    posts_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    paginator = Paginator(posts_list, 1) # change this to accommodate number of posts per page
+    page = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post_list.html',
+                  {'base_dir': settings.BASE_DIR, 'posts': posts, 'page': page})
