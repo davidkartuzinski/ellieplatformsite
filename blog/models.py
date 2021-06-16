904266d371b8f9ecb8353ce739f422241857f119
davidkartuzinski <david@kaidawei.me>
2021-06-16 11:43:07 +0200

    only some of this works. I don't know which part but the whole thing is broken.  Specifically the whole thing is getting the author page to show up with the posts of the author. I am missing something here. This is the URL I am trying: http://localhost:8000/blog/author/david but that is not working. Now it seems I have the URL bringing the view correctly. But I think that is a fake out. I am however getting this error: Exception Type: TypeError at /blog/author/david Exception Value: author_post_list() got an unexpected keyword argument 'author'

--- blog/models.py
+++ blog/models.py
@@ -17,17 +17,26 @@ class Profile(models.Model):
     bio = RichTextField(null=True, max_length=500)
     profile_pic = models.ImageField(default='default-profile-pic.jpeg', upload_to='profiles-pics')
     # Social Media
-    twitter_bio_handle = models.CharField(max_length=255, null=False, blank=True, default="@open_apprentice")
-    youtube_handle = models.CharField(max_length=255, null=False, blank=True)
-    linkedin_profile_url = models.CharField(max_length=255, null=False, blank=True)
-    instagram_handle = models.CharField(max_length=255, null=False, blank=True, default="@open_apprentice")
-    github_url = models.CharField(max_length=255, null=False, blank=True)
-    author_website = models.CharField(max_length=255, null=False, blank=True, default="https://ellieplatform.org")
+    twitter_bio_handle = models.CharField(max_length=255, null=False, blank=True, default="@open_apprentice",
+                                          help_text="Enter just your handle, example, 'superbob'.")
+    youtube_handle = models.CharField(max_length=255, null=False, blank=True,
+                                      help_text="Enter just your username, example, 'bobbarker'.")
+    linkedin_profile_url = models.CharField(max_length=255, null=False, blank=True,
+                                            help_text="Enter just your username, example, 'bobbarker'.")
+    instagram_handle = models.CharField(max_length=255, null=False, blank=True, default="@open_apprentice",
+                                        help_text="Enter just your handle, example, 'superbob'.")
+    github_url = models.CharField(max_length=255, null=False, blank=True,
+                                  help_text="Enter just your username, example, 'superbob'.")
+    author_website = models.CharField(max_length=255, null=False, blank=True, default="https://ellieplatform.org",
+                                      help_text="Enter your full website URL with https:// ")
 
     class Meta:
         verbose_name = "Profile"
         verbose_name_plural = "Profiles"
         ordering = ['user']
+
+    def get_absolute_url(self):
+        return reverse('author', kwargs={'pk': self.pk, 'author': self.user.username})
 
     def __str__(self):
         return str(self.user)
@@ -54,11 +63,11 @@ class Category(models.Model):
         verbose_name_plural = "Categories"
         ordering = ['name']
 
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'pk': self.pk, 'slug': self.slug})
+
     def __str__(self):
         return self.name
-
-    def get_absolute_url(self):
-        return reverse('home', kwargs={'pk': self.pk, 'slug': self.slug})
 
 
 # Post Model
@@ -68,7 +77,7 @@ class Post(models.Model):
         ('published', 'Published'),
     )
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=60, unique=True)
     category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,9 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
+
     path('', views.post_list, name='post_list'),
     path('<slug:slug>/', views.blog_post, name="blog_post"),
-    path('category/<slug:slug>/', views.category, name='category'),
-    path('author', views.author_post_list, name='author_post_list'),
-
+    path('category/<slug:slug>/', views.category_post_list, name='category'),
+    path('author/<str:author>', views.author_post_list, name='author'),
 ]
